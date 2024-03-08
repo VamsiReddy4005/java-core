4b75ae5d8daadf484186fb38212b68c17fbe23f0
VamsiReddy4005 <109601609+VamsiReddy4005@users.noreply.github.com>
2024-03-08 15:47:23 +0530

    core java

--- src/com/learning/core/day3/D03P06.java
+++ src/com/learning/core/day3/D03P06.java
@@ -5,19 +5,11 @@ import java.util.Scanner;
 public class D03P06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Input String
         System.out.print("Enter a string: ");
         String inputString = scanner.nextLine();
-
-        // Input value of 'n'
         System.out.print("Enter the value of 'n': ");
         int n = scanner.nextInt();
-
-        // Count the last 'n' vowels
         String lastNVowels = countLastNVowels(inputString, n);
-
-        // Output
         System.out.println("Output: " + lastNVowels);
     }
 
@@ -29,16 +21,16 @@ public class D03P06 {
             char currentChar = inputString.charAt(i);
 
             if (isVowel(currentChar)) {
-                lastNVowels.insert(0, currentChar); // Add vowels in reverse order
+                lastNVowels.insert(0, currentChar); 
                 vowelCount++;
 
                 if (vowelCount == n) {
-                    break; // Stop when 'n' vowels are found
+                    break; 
                 }
             }
         }
 
-        // Check for mismatch in vowel count
+       
         if (vowelCount < n) {
             return "Mismatch in Vowel Count";
         }
